0ef1953f8d6b7f4667fa2e7a4062a12391684264
manarHasan <manar.hasan62@gmail.com>
2021-01-26 14:05:48 +0200

    destruct

---
+++ MERN Stack/JavaScript/Destructing.js
@@ -0,0 +1,61 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// output 1: Tesla
+// output 2: Mercedes
+//////////////////////////////////////////////
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// output: error Undefined variable
+//////////////////////////////////////////////
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// output 1: 12345
+// output 2: undefined
+///////////////////////////////////////////////
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// output 1: false
+// output 2: true
+///////////////////////////////////////////////
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// output 1: value
+// output 2: [1, 5, 1, 8, 3, 3]
+// output 3: 1
+// output 4: 5
+
+
