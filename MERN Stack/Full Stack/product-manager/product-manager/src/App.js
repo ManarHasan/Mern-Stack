b91669a79945635c84de5d44b75679bd8b624a3e
manarHasan <manar.hasan62@gmail.com>
2021-02-10 14:15:33 +0200

    product delete component

--- MERN Stack/Full Stack/product-manager/product-manager/src/App.js
+++ MERN Stack/Full Stack/product-manager/product-manager/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Main from './Main';
 import { Router } from '@reach/router';
@@ -6,12 +7,26 @@ import Product from './components/Product';
 import Update from './components/Update';
 
 function App() {
+  const [products, setProducts] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+      axios.get("http://localhost:8000/api/products")
+          .then(res => {
+              setProducts(res.data);
+              setLoaded(true);
+          })
+  })
+  
+  const removeFromDom = productId => {
+    setProducts(products.filter(product => product._id != productId))
+  }
 
   return (
     <div className="App">
       <Router>
-        <Main path="products/"/>
-        <Product path="product/:id" />
+        {loaded && <Main path="products/" products={products} removeFromDom={removeFromDom}/>}
+        <Product path="product/:id" removeFromDom={removeFromDom} />
         <Update path="products/:id/edit" />
       </Router>
     </div>
--- MERN Stack/Full Stack/product-manager/product-manager/src/Main.js
+++ MERN Stack/Full Stack/product-manager/product-manager/src/Main.js
@@ -3,27 +3,13 @@ import React, { useState, useEffect } from 'react'
 import DisplayProducts from './components/DisplayProducts';
 import ProductForm from './components/ProductForm';
 
-export default () => {
-    const [products, setProducts] = useState("");
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-        axios.get("http://localhost:8000/api/products")
-            .then(res => {
-                setProducts(res.data);
-                setLoaded(true);
-            })
-    })
-
-    const removeFromDom = productId => {
-        setProducts(products.filter(product => product._id != productId))
-    }
-
+export default (props) => {
+    
     return (
         <div>
             <ProductForm />
             <hr/>
-            {loaded && <DisplayProducts products={products} removeFromDom={removeFromDom}/>}
+            <DisplayProducts products={props.products} removeFromDom={props.removeFromDom}/>
         </div>
     )
 }
